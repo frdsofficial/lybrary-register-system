eff8ad72e0d76ae3e37751dec8d5f133f3ba8548
frdsofficial <frds.official@gmail.com>
2024-02-10 05:16:30 -0300

    Library Register System

---
+++ libraryRegister.rb
@@ -0,0 +1,8 @@
+puts "Seja bem vindo ao sitema de cadastro da nossa biblioteca."
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Cadastro de  #{nome} #{sobrenome} (#{idade} anos), realizado com sucesso!"
